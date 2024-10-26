6a69642336c2bec811d5908084ca4dad2e259bcd
John Danner <john@danners.org>
2024-10-26 13:02:49 -0700

    Restructure project: move forms.py and models.py into weightlifting_tracker directory, update imports

--- weightlifting_tracker/models.py
+++ weightlifting_tracker/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from app import db, login_manager
+from weightlifting_tracker.app import db, login_manager
 from flask_login import UserMixin
 
 @login_manager.user_loader
