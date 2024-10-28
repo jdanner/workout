068c29bb335c042262e30ccb5f7cd1c9c1cf2402
John Danner <john@danners.org>
2024-10-28 09:28:19 -0700

    render

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
+import routes
 
 app = Flask(__name__)
 # Use environment variables for sensitive data
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from __init__ import app as application
 
 if __name__ == "__main__":
